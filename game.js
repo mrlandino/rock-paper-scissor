b08b39c957a076cf24bb70e332f1fdfd1759e3ad
Ross Landino <mrlandino@gmail.com>
2022-03-05 13:57:33 -0700

    Complete classic game winner outcome functionality

--- game.js
+++ game.js
@@ -13,66 +13,83 @@ class Game {
   }
 
   classicRPSWinner(player1, player2){
-    var rock = 1;
-    var paper = 2;
-    var scissors = 3;
-    var currentWinner = "";
+    if (player1 === "rock") {
+      player1 = 1;
+    } else if (player1 === "paper") {
+      player1 = 2;
+    } else {
+      player1 = 3;
+    }
 
+    if (player2 === "rock") {
+      player2 = 1;
+    } else if (player2 === "paper") {
+      player2 = 2;
+    } else {
+      player2 = 3;
+    }
+
+    var currentWinner = "";
+    
     if (player1 + player2 === 4 && player1 !== player2) {
-      currentWinner = "rock";
+      currentWinner = 1;
     } else if (player1 + player2 === 3 && player1 !== player2) {
-      currentWinner = "paper";
-    } else if (player1 + player2 === 2 && player1 !== player2) {
-      currentWinner = "scissors";
+      currentWinner = 2;
+    } else if (player1 + player2 === 5 && player1 !== player2) {
+      currentWinner = 3;
     } else {
       currentWinner = "tie";
     }
 
     if (player1 === currentWinner) {
       this.player1.wins++;
-      return "player 1 wins!!";
+      console.log("player 1 wins!!");
     } else if (player2 === currentWinner){
       this.player2.wins++;
-      return "player 2 wins!!";
+      console.log("player 2 wins!!");
     } else {
-      return "its a DRAW!!";
+      console.log("its a DRAW!!");
     }
   }
 
-  modernRPSWinner(player1, player2){
-    var rock > scissors || volcano;
-    var paper > rock || tornado;
-    var scissors > paper || volcano;
-    var volcano > paper || tornado;
-    var tornado > scissors || rock;
-
-    if (player1 > player2) {
-      return "player1 wins";
-      //need to add (wins count, message)??
-      //refresh fighter choice??
-    }
-    if (player1 < player2) {
-      return "player1 wins";
-      //need to add (wins count, message)??
-      //refresh fighter choice??
-    }
-    if (player1 === player2) {
-      return "ITS A DRAW!!"
-    }
-  }
+  // modernRPSWinner(player1, player2){
+  //   var rock > scissors || volcano;
+  //   var paper > rock || tornado;
+  //   var scissors > paper || volcano;
+  //   var volcano > paper || tornado;
+  //   var tornado > scissors || rock;
+  //
+  //   if (player1 > player2) {
+  //     return "player1 wins";
+  //     //need to add (wins count, message)??
+  //     //refresh fighter choice??
+  //   }
+  //   if (player1 < player2) {
+  //     return "player1 wins";
+  //     //need to add (wins count, message)??
+  //     //refresh fighter choice??
+  //   }
+  //   if (player1 === player2) {
+  //     return "ITS A DRAW!!"
+  //   }
+  // }
 
   checkGameWinner(gameType, player1, player2){
     //player1 choice agains player 2 choice with the game type to compare
     if (gameType === "classic"){
-      classicRPSWinner(player1, player2);
+      this.classicRPSWinner(player1, player2);
     }
     if (gameType === "modern"){
-      modernRPSWinner(player1, player2);
+      this.modernRPSWinner(player1, player2);
     }
   }
 
+  computerRandomFighter(){
+    var fighterArray = ["rock", "paper", "scissors"];
+    return fighterArray[Math.floor(Math.random() * fighterArray.length)];
+  }
   gameChange(){
-    
+
   }
 
   scoreReset(){

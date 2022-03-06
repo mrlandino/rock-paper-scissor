2b5712628b32ae1653bd9369e030eb2ee42b8a2f
Ross Landino <mrlandino@gmail.com>
2022-03-05 18:32:43 -0700

    Refactor fighter randomizer

--- game.js
+++ game.js
@@ -124,18 +124,19 @@ class Game {
     }
   }
 
-  computerRandomFighterClassic(){
-    var fighterArray = ["rock", "paper", "scissors"];
-    return fighterArray[Math.floor(Math.random() * fighterArray.length)];
+  computerRandomFighter(gameType){
+    if (gameType === "classic"){
+      var fighterArray = ["rock", "paper", "scissors"];
+      return fighterArray[Math.floor(Math.random() * fighterArray.length)];
+    }
+    if (gameType === "modern") {
+      var fighterArray = ["rock", "paper", "scissors", "volcano", "tornado"];
+      return fighterArray[Math.floor(Math.random() * fighterArray.length)];
+    }
   }
 
-  computerRandomFighterModern(){
-    var fighterArray = ["rock", "paper", "scissors", "volcano", "tornado"];
-    return fighterArray[Math.floor(Math.random() * fighterArray.length)];
-  }
-
-  gameChange(){
-
+  newGame(){
+    document.location.reload();
   }
 
   scoreReset(){
